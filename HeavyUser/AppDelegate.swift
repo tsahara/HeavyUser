9f3a7fff347cea101fd4e46ab0b15fe0e79bce3b
Tomoyuki Sahara <sahara@caddr.net>
2017-09-26 21:23:30 +0900

    Initial Commit

---
+++ HeavyUser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HeavyUser
+//
+//  Created by Tomoyuki Sahara on 2017/09/26.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HeavyUser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeavyUser
+//
+//  Created by Tomoyuki Sahara on 2017/09/26.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
