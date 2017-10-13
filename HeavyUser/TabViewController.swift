63b47b8b1f2be665cfff02703273d9d14f793384
Tomoyuki Sahara <sahara@caddr.net>
2017-10-13 23:45:03 +0900

    add TabView.

---
+++ HeavyUser/TabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewController.swift
+//  HeavyUser
+//
+//  Created by Tomoyuki Sahara on 2017/10/13.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+class TabViewController: NSTabViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("tab view load")
+
+//        tabView.controlTint = .graphiteControlTint
+    }
+}
